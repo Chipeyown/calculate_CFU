2fde357e3ba2ac92a70b26a9ed7adce26f4b83dd
Chipeyown <49014853+Chipeyown@users.noreply.github.com>
2024-05-06 18:10:11 +0800

    Update calculate_CFU.py

--- calculate_CFU.py
+++ calculate_CFU.py
@@ -3,14 +3,14 @@ from skimage import io
 from skimage.transform import resize
 
 
-##############################################圈出边界
+##############################################鼠标左键点击，标记数过的菌落
 def OnPress(event):
     if event.button == 1:
         dots.append((event.xdata, event.ydata))
         ax.scatter(dots[-1][0], dots[-1][1], color='red', s=10)
         fig.canvas.draw()
 
-##############################################单个计算
+##############################################
 pic = input('please input picture links:')
 img = io.imread(pic)
 #img = resize(img, (img.shape[0] / 3, img.shape[1] /3),anti_aliasing=True)
@@ -21,8 +21,8 @@ while True:
     ax = fig.add_subplot(111)
     ax.imshow(img)
     plt.show()
-    print(len(dots))
-    for dot in dots:
+    print(len(dots))#自动显示已数过的菌落个数
+    for dot in dots:#数过的菌落用蓝色标记
         for i in range(int(dot[1])-3,int(dot[1])+3):
             for j in range(int(dot[0])-3,int(dot[0])+3):
                 img[i,j]=[0,0,1]
